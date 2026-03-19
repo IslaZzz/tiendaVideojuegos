824c287ff3c0344d5d913acd7cceda9ae65293df
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2026-03-19 15:44:46 -0700

    correccion a posters y detail

--- app/src/main/java/islas/abril/tiendavideojuegos/dummies/VideojuegosList.kt
+++ app/src/main/java/islas/abril/tiendavideojuegos/dummies/VideojuegosList.kt
@@ -13,14 +13,14 @@ var listaDeJuegos = listOf<Videojuego>(
        60.99,
        "Accion",
        "Metroid Dread, es un videojuego de acción-aventura de 2021 de la serie Metroid, desarrollado por MercurySteam en colaboración con la filial Nintendo EPD y publicado por Nintendo para la consola Nintendo Switch.",
-       R.drawable.posterMetroid
+       R.drawable.postermetroid
        ),
     Videojuego(
         "DOOM Eternal",
         59.99,
         "Shooter",
         "DOOM Eternal es un videojuego de disparos en primera persona de 2020 desarrollado por id Software y publicado por Bethesda. Es la secuela de DOOM (2016) y destaca por su combate rápido, violento y estratégico contra demonios.",
-        R.drawable.posterDoom
+        R.drawable.posterdoom
     ),
 
     Videojuego(
@@ -28,7 +28,7 @@ var listaDeJuegos = listOf<Videojuego>(
         49.99,
         "Terror",
         "Silent Hill 2 es un videojuego de terror psicológico desarrollado por Konami. Sigue la historia de James Sunderland mientras explora el misterioso pueblo de Silent Hill en busca de su esposa.",
-        R.drawable.posterSilentHill
+        R.drawable.postersilenthill
     ),
 
     Videojuego(
@@ -36,7 +36,7 @@ var listaDeJuegos = listOf<Videojuego>(
         39.99,
         "Terror",
         "Resident Evil 4 es un videojuego de acción y survival horror desarrollado por Capcom. Sigue a Leon S. Kennedy en una misión para rescatar a la hija del presidente en una aldea infestada.",
-        R.drawable.posterResidentEvil
+        R.drawable.posterresidentevil
     ),
 
     Videojuego(
@@ -44,7 +44,7 @@ var listaDeJuegos = listOf<Videojuego>(
         26.95,
         "Sandbox",
         "Minecraft es un videojuego de construcción y aventura desarrollado por Mojang donde los jugadores pueden explorar, recolectar recursos y crear mundos en un entorno generado de forma procedural.",
-        R.drawable.posterMinecraft
+        R.drawable.posterminecraft
     ),
 
     Videojuego(
@@ -52,10 +52,14 @@ var listaDeJuegos = listOf<Videojuego>(
         59.99,
         "Aventura",
         "The Legend of Zelda: Breath of the Wild es un videojuego de mundo abierto desarrollado por Nintendo donde el jugador explora Hyrule con total libertad, resolviendo acertijos y enfrentando enemigos.",
-        R.drawable.posterZelda
+        R.drawable.posterzelda
     )
 )
 
 fun obtenerVideojuegos(): List<Videojuego>{
     return listaDeJuegos
  }
+
+fun obtenerVideojuegoRandom(): Videojuego{
+    return listaDeJuegos.random()
+}
--- app/src/main/java/islas/abril/tiendavideojuegos/ui/screens/VideogameDetail.kt
+++ app/src/main/java/islas/abril/tiendavideojuegos/ui/screens/VideogameDetail.kt
@@ -1,2 +1,36 @@
 package islas.abril.tiendavideojuegos.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import islas.abril.tiendavideojuegos.dummies.obtenerVideojuegoRandom
+import islas.abril.tiendavideojuegos.model.Videojuego
+
+
+@Preview (showBackground = true)
+@Composable
+fun showVideogameDetail(){
+    videogameDetail(obtenerVideojuegoRandom())
+}
+
+@Composable
+fun videogameDetail(videojuego: Videojuego){
+
+    Column(modifier = Modifier.fillMaxSize()){
+        Row(modifier = Modifier.padding(top=50.dp, start=25.dp)){
+            Text(videojuego.nombre,
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
+}
