dd0e8d124a64a403124a133748e4d2fa89fe5b54
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2026-03-19 14:59:51 -0700

    proyecto creado

---
+++ app/src/main/java/islas/abril/tiendavideojuegos/MainActivity.kt
@@ -0,0 +1,47 @@
+package islas.abril.tiendavideojuegos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import islas.abril.tiendavideojuegos.ui.theme.TiendaVideojuegosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TiendaVideojuegosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TiendaVideojuegosTheme {
+        Greeting("Android")
+    }
+}
