acba24afc75191d018619253683d8c16ae6ed0a9
IslaZzz <99843612+IslaZzz@users.noreply.github.com>
2026-03-19 15:05:17 -0700

    clase videojuego

---
+++ app/src/main/java/islas/abril/tiendavideojuegos/model/videojuegos.kt
@@ -0,0 +1,10 @@
+package islas.abril.tiendavideojuegos.model
+
+data class videojuegos(
+
+    val nombre: String,
+    var precio: Double,
+    val categoria: String,
+    val descripcion: String
+
+)
